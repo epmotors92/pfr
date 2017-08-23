ea4b260ceb1999b4b00c81b67a56348cb369c669
epmotors92 <30411731+epmotors92@users.noreply.github.com>
2017-08-23 15:03:57 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$("#contact-message-success").hide();
+});
+
+// $(document).ready(function() {
+	// $("#contact-submit").click(function() {
+		// $("#contact-message-success").show();
+		// $("#contact-message-success").fadeOut(5000);
+	// });
+// });
